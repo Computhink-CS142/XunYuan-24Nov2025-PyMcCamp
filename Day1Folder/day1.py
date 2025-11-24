c2118e9aedd74b728b9c83dcef487d8354eff059
Computhink-CS142 <cs142@computhink.com.sg>
2025-11-24 08:32:39 +0000

    Nov 24, 2025, 8:32 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,40 +1,56 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
+
 
 ########################################################################
 # Task 1:
-
-
+# print("chicken banana chicken banana chicken banana banana banana")
 
 ########################################################################
 # Task 2:
-
-
-
+# print("xun yuan")
+# print("turning 11")
+# print("hong wen")
+# print("things")
 ########################################################################
 # Task 3:
-
-
-
+# animal1="chicken"
+# animal2="cow"
+# animal3="sheep"
+# print(animal1)
+# print(animal2)
+# print(animal3)
 ########################################################################
 # Task 4:
-
+# variable1 = 3
+# variable2 = 2
+# print (variable1 + variable2)
+# print(variable1 * variable2)
+# print(variable1 / variable2)
 
 
 ########################################################################
 # Task 5:
-
-
+# name = "duck"
+# food = "duck food"
+# print("my name is " + name)
+# print("i like " + food)
 
 ########################################################################
 # Task 6:
-
-
+# def intro():
+    # print("xun yuan")
+    # print("turning 11")
+    # print("hong wen")
+    # print("things")
+# intro()
 
 ########################################################################
 # Task 7:
-
+# def addition(number1 , number2):
+    # print(number1 + number2)
+# addition(6,7)
 
 
 ########################################################################
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,76 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+
 ################# Function Section #############################
 
+# teleport
+def come():
+   agent.teleport_to_player()
 
+# moving
+def forward(steps):
+    agent.move(FORWARD, steps)
+
+def back(steps):
+    agent.move(BACK, steps)
+
+def up(steps):
+    agent.move(UP, steps)
+
+def down(steps):
+    agent.move(DOWN, steps)
+
+# turning
+def left():
+    agent.turn(TurnDirection.LEFT)
+
+def right():
+    agent.turn(TurnDirection.RIGHT)
+
+#obby1
+def movestep():
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 4)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+# mf 4
+# lt
+# mf 5
+# rt
+# mf 3
+# up n fw 3
+# fw 1
+# dw n fw 3
 ################## On Chat Commands Section #####################
+
+# teleport
+player.on_chat("c", come)
+
+# moving
+player.on_chat("mf", forward)
+
+player.on_chat("bk", back)
+
+player.on_chat("up", up)
+
+player.on_chat("dw", down)
+
+# turning
+player.on_chat("lt", left)
+
+player.on_chat("rt", right)
+
+#obby1
+
+player.on_chat("obby", movestep)
+    
